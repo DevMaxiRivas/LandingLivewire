c4d27158a32f0bdf611216349d8431d7c7af2921
MR PC <maximilianorivas01@gmail.com>
2025-07-02 20:55:31 -0300

    pagina showblog

--- app/Livewire/ShowBlog.php
+++ app/Livewire/ShowBlog.php
@@ -2,16 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
+use App\Models\Category;
 use Livewire\Component;
 
 class ShowBlog extends Component
 {
     public function render()
     {
-        // $blogs = Article::orderBy('created_at', 'desc')
-        //     ->with(['category', 'user'])
-        //     ->paginate(10);
+        $blogs = Article::orderBy('created_at', 'desc')->get();
+        $categories = Category::where('status', 1)->get();
 
-        return view('livewire.show-blog');
+        return view('livewire.show-blog', compact('blogs', 'categories'));
     }
 }
